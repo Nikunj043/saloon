a726182ab1a7379c7b37310738e08838ac98b432
Nikunj043 <166729790+Nikunj043@users.noreply.github.com>
2024-05-05 22:13:28 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "msmsdb");
+$con=mysqli_connect("msmsdb.mysql.database.azure.com", "ikun", "@Qwerty123", "msmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
